c9d00ec626b5fcd7c6a6a776c2d9bddd9d967a0d
Elliot <eschem58@gmail.com>
2024-02-29 13:41:05 -0600

    env

--- src/env.js
+++ src/env.js
@@ -6,16 +6,14 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-
   server: {
-    /*DATABASE_URL: z
+    DATABASE_URL: z
       .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        (str) => !str.includes(""),
         "You forgot to change the default URL",
       ),
-			*/
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -25,7 +23,7 @@ export const env = createEnv({
    * Specify your client-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
-  /
+   */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
@@ -33,7 +31,7 @@ export const env = createEnv({
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
-  /
+   */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
@@ -42,12 +40,11 @@ export const env = createEnv({
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
-  /
+   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   /**
    * Makes it so that empty strings are treated as undefined.
    * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
-   
-  emptyStringAsUndefined: true,
-	*/
+   */
+  emptyStringAsUndefined: false,
 });
